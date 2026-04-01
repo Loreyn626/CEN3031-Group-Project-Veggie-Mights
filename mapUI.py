afbbdb3fa4e0b42a374e2e6104d08f484be8152f
narutyunov <narutyunov@ufl.edu>
2026-04-01 18:14:09 -0400

    Reworked data organization logic to support all columns from the spreadsheet + Added a dropdown to show "Daily healthy diet costs" and "Calculated total costs"

--- mapUI.py
+++ mapUI.py
@@ -3,16 +3,51 @@ from data.classes import *
 from data.parsing import countries
 
 country_names = [] # For locations
-all_annual_costs = {} # For annual costs
+
+# Lists based on the data columns in the spreadsheet
+all_annual_costs = [[], []] # Daily annual healthy diet and calculated total costs in USD
+all_veg_fruit_costs = [[], [], []] # Vegetable costs, fruit costs, vegetable + fruit total costs in USD
 
 for country in countries:
    country_names.append(country.name)
 
-   all_annual_costs[country.name] = [] # Initialize array
-   for cost in country.annualCosts:
-      # Needs to be converted to float, wasn't working properly otherwise
-      all_annual_costs[country.name].append(float(cost))
+   # Costs need to be converted to floats, otherwise the data won't properly display
+   # Initialize empty "country_cost" list to prevent "index out of range" errors when adding data
 
+   country_costs = []
+   for cost in country.costPPPs:
+      country_costs.append(float(cost))
+   all_annual_costs[0].append(country_costs)
+
+   country_costs = []
+   for cost in country.annualCosts:
+      country_costs.append(float(cost))
+   all_annual_costs[1].append(country_costs)
+
+   # Vegetable costs (index 0), fruit costs (index 1), vegetable + fruit total costs (index 2)
+   country_costs = []
+   for cost in country.vegetablesPPPs:
+      if cost == "":
+         country_costs.append(None)
+      else:
+         country_costs.append(float(cost))
+   all_veg_fruit_costs[0].append(country_costs)
+
+   country_costs = []
+   for cost in country.fruitsPPPs:
+      if cost == "":
+         country_costs.append(None)
+      else:
+         country_costs.append(float(cost))
+   all_veg_fruit_costs[1].append(country_costs)
+
+   country_costs = []
+   for cost in country.totalFoodCosts:
+      if cost == "":
+         country_costs.append(None)
+      else:
+         country_costs.append(float(cost))
+   all_veg_fruit_costs[2].append(country_costs)
 
 def plotMap():
    cost_categories = ['Low', 'Medium', 'High']
@@ -20,56 +55,65 @@ def plotMap():
 
    fig = go.Figure() # Initialize graph object
 
-   # ========= ANNUAL COST + SLIDERS PER YEAR =========
-   yearly_z_values = []
-   for i in range(8): # 8 years: 2017, 2018, 2019, 2020, 2021, 2022, 2023, 2024
+   # ========= DAILY HEALTHY DIET & ANNUAL TOTAL COSTS + SLIDERS PER YEAR =========
+   for metric_index, metric_data in enumerate(all_annual_costs):
+      # For Choropleth z-value parameter
+      all_z_values = []
+
       # Each year needs different z-values
-      year_z_values = []
+      for i in range(8): # 8 years: 2017, 2018, 2019, 2020, 2021, 2022, 2023, 2024
+         year_z_values = []
 
-      # Add all costs for that specific year
-      for name in country_names:
-         costs = all_annual_costs[name]
+         # Add all costs for that specific year
+         for country_index in range(len(country_names)):
+            costs = metric_data[country_index] # Each country's multi-year list
 
-         if i < len(costs):
-            cost = costs[i]
-         else:
-            cost = None
+            if i < len(costs):
+               cost = costs[i]
+            else:
+               cost = None # Placeholder if missing data (doesn't show on map)
 
-         year_z_values.append(cost)
-      
-      # Add array of each country's annual cost for the year to a yearly array
-      yearly_z_values.append(year_z_values)
+            # Add cost data to the specific year's list
+            year_z_values.append(cost)
 
-   # Determine min and max for the specific year
-   # Can't just use min() or max() on z_values because it doesn't work with "None" values
-   yearly_costs = []
+         # Add list of each country's annual cost for the year to a yearly list
+         all_z_values.append(year_z_values)
 
-   for year in yearly_z_values:
-      for value in year:
-         if value is not None:
-            yearly_costs.append(value)
+      # Determine min and max for the specific year
+      # Can't just use min() or max() on z_values because it doesn't work with "None" values
+      yearly_costs = []
 
-   # zmin & zmax is the lowest and highest value of ALL costs over ALL years
-   # This helps keep colorscale consistent between years and when we trace 8 times (1 trace per year)
-   zmin = min(yearly_costs)
-   zmax = max(yearly_costs)
+      for year in all_z_values:
+         for value in year:
+            if value is not None:
+               yearly_costs.append(value)
 
-   # go.Choropleth is where data is actually being inserted
-   for i in range(8):
-      fig.add_trace(go.Choropleth(
-         # insert country names here as an array
-         locations=country_names,
-         locationmode='country names',
-         # Display all annual costs for now before adding more info later
-         z=yearly_z_values[i],
-         # Color scaling, 0% of the way to max is green, 50% = orange, 100% = red
-         colorscale=[[0.0, colors[0]],[0.5, colors[1]],[1.0, colors[2]]],
-         showscale=True,
-         # this contains some data being displayed
-         name=str(i + 2017),
-         zmin=zmin,
-         zmax=zmax
-      ))
+      # zmin & zmax is the lowest and highest value of ALL costs over ALL years
+      # This helps keep colorscale consistent between years and when we trace 8 times (1 trace per year)
+      zmin = min(yearly_costs)
+      zmax = max(yearly_costs)
+
+      if metric_index == 0:
+         metric_name = "Daily healthy diet costs"
+      else:
+         metric_name = "Calculated total annual costs"
+
+      # go.Choropleth is where data is actually being inserted
+      for i in range(8):
+         fig.add_trace(go.Choropleth(
+            # insert country names here as an array
+            locations=country_names,
+            locationmode='country names',
+            # Display all annual costs for now before adding more info later
+            z=all_z_values[i],
+            # Color scaling, 0% of the way to max is green, 50% = orange, 100% = red
+            colorscale=[[0.0, colors[0]],[0.5, colors[1]],[1.0, colors[2]]],
+            showscale=True,
+            # this contains some data being displayed
+            name=metric_name+str(i + 2017),
+            zmin=zmin,
+            zmax=zmax
+         ))
 
    # go.Scattergeo function is only being used to incorporate figure labels for the colors
    # don't insert any data here
@@ -88,21 +132,44 @@ def plotMap():
    # For slider creation and labeling
    steps = []
    for i in range(8): # 8 different years
-      # Only show one year's map at a time, prevent overlap
-      visible_list = []
+      # Only show one year at a time from one data metric, prevent overlap
+
+      # ============= DAILY HEALTHY DIET COSTS =============
+      visible_daily = []
 
       for j in range(8):
-         # Include the legend in all years
-         visible_list.extend([True] * len(cost_categories))
-
          if j == i:
-            visible_list.append(True)
+            visible_daily.append(True)
          else:
-            visible_list.append(False)
+            visible_daily.append(False)
 
+      # Add slots for hiding annual total data
+      for j in range(8):
+         visible_daily.append(False)
+
+      # Include the legend in all years
+      visible_daily.extend([True] * len(cost_categories))
+
+      # ============= CALCULATED TOTAL ANNUAL COSTS =============
+      visible_annual = []
+
+      for j in range(8):
+         if j == i:
+            visible_annual.append(True)
+         else:
+            visible_annual.append(False)
+
+      # Add slots for hiding daily healthy diet costs
+      for j in range(8):
+         visible_annual.append(False)
+
+      # Include the legend in all years
+      visible_annual.extend([True] * len(cost_categories))
+
+      # Update slider steps
       step = dict(
          method="update", # Updates map
-         args=[{"visible": visible_list}],
+         args=[{"visible": visible_daily}],
          label=str(i + 2017)
       )
       steps.append(step)
@@ -134,7 +201,25 @@ def plotMap():
          x=0.89,
          itemclick=False,
          itemdoubleclick=False
-      )
+      ),
+      # Dropdown menu for different data columns
+      updatemenus=[
+         dict(
+            buttons=[
+               dict(
+                  args=[{"visible": visible_daily}],
+                  label="Daily Healthy Diet Costs",
+                  method="update"
+               ),
+               dict(
+                  args=[{"visible": visible_annual}],
+                  label="Calculated Total Costs",
+                  method="update"
+               )
+            ],
+            direction="down"
+         )
+      ]
    )
 
    return fig
